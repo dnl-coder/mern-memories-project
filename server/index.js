6c8377a738790a4f759fee04258e3e1b12eb53db
Daniel Lingan <dalc.2506@gmail.com>
2021-10-07 11:59:38 -0500

    [ UPDATE ] Implementing redux

--- server/index.js
+++ server/index.js
@@ -7,11 +7,13 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECT_URL = 'mongodb+srv://dnl_250694:dnl_250694@cluster0.iedtn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
