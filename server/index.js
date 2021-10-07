eede7e47ffae6073e72b96b45baef12fe5269530
Daniel Lingan <dalc.2506@gmail.com>
2021-10-06 22:17:02 -0500

    [ UPDATE ] Routing backend

--- server/index.js
+++ server/index.js
@@ -3,8 +3,21 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+const CONNECT_URL = 'mongodb+srv://dnl_250694:dnl_250694@cluster0.iedtn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECT_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+mongoose.set('debug', true);
